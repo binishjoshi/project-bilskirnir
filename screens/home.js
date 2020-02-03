be931ab33483e2d98d35320adf79b28781f5bf16
Binish Joshi <beeneez6233@gmail.com>
2020-02-02 23:22:53 -0500

    Removed react-native-svg, added react-native-progress-circle, positioned the progress circle

--- screens/home.js
+++ screens/home.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import {
+  Dimensions,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
 
-import ProgressCircle from '../components/progressCircle';
+import ProgressCircle from 'react-native-progress-circle';
 import ProgressContainer from '../components/progressContainer';
 import OptionsContainer from '../components/optionsContainer';
+
+const { width } = Dimensions.get('window');
+const radius = width - 270;
 
 const Home = () => {
   return (
     <View style={styles.homeContainer}>
+
       <View style={styles.currentLevelContainer}>
-        <ProgressCircle />
+        <View style={styles.circleContainer}>
+          <ProgressCircle
+                percent={69}
+                radius={radius}
+                borderWidth={18}
+                color="#3399FF"
+                shadowColor="#999"
+                bgColor="#fff"
+            >
+              <Text>69 %</Text>
+            </ProgressCircle>
+        </View>
+        <View style={styles.circleDetail}>
+          <Text>Details about circle</Text>
+        </View>
       </View>
+
       <View style={styles.progressContainer}>
         <ProgressContainer />
       </View>
+      
       <View style={styles.optionsContainer}>
         <OptionsContainer />
       </View>
@@ -32,7 +53,22 @@ const styles = StyleSheet.create({
   },
   currentLevelContainer:{
     flex: 4,
+    alignItems: 'center',
     backgroundColor: '#49d8d4',
+  },
+  circleContainer:{
+    flex: 8,
+    backgroundColor: 'rgb(123, 99, 216)',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  circleUp:{
+    flex: 3,
+    backgroundColor: 'rgb(12, 69, 132)'    
+  },
+  circleDetail: {
+    flex: 2,
+    backgroundColor: 'rgb(116, 159, 60)',
   },
   progressContainer: {
     flex: 2,
