d6720b0460a9fda4133bed4b06e4211179ef328b
Binish Joshi <beeneez6233@gmail.com>
2020-02-03 00:36:41 -0500

    Wireframed Progress Bars

--- components/progress.jsx
+++ components/progress.jsx
@@ -1,21 +1,55 @@
 import React from 'react';
 import {
+  Dimensions,
+  Platform,
+  ProgressBarAndroid,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
 
-const Progress = () => {
+const { width } = Dimensions.get('window');
+const progressLength = width / 2 - 20;
+
+const Progress = ({ title }) => {
   return (
-    <View style={styles.progress}>
-      <Text>This is a progress</Text>
+    <View style={styles.progressContainer}>
+
+      <View style={styles.progressTitle}>
+        <Text>{ title }</Text>
+      </View>
+
+      <View style={styles.bar}>
+        { Platform.OS === 'android' ? 
+          <ProgressBarAndroid
+            styleAttr="Horizontal"
+            indeterminate={false}
+            progress={0.19}
+            style={styles.progress}
+          />:
+          null
+        }        
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  progressContainer: {
+    backgroundColor: 'rgb(56,26,100)',
+    width: progressLength,
+    borderRadius: 6,
+  },
+  progressTitle: {
+    backgroundColor: 'rgb(56,26,200)',
+    padding: 5,
+  },
+  bar: {
+    backgroundColor: 'rgb(79, 91, 39)',
+  },
   progress: {
-    backgroundColor: '#d60d68',
+    width: '90%',
+    height: 40,
   }
 });
 
--- components/progressContainer.jsx
+++ components/progressContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  FlatList,
   StyleSheet,
   Text,
   View,
@@ -7,19 +8,63 @@ import {
 
 import Progress from './progress';
 
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd-145571e29d72',
+    title: 'Third Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Forth Item',
+  },
+  {
+    id: '586940f-3da1-471f-bd96-145571e29d72',
+    title: 'Fifth Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd6-145571e29d72',
+    title: 'Sixth Item',
+  },
+  {
+    id: '58694a0f-3da1-471\bd96-145571e29d72',
+    title: 'Seventh Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96571e29d72',
+    title: 'Eighth Item',
+  },
+];
+
 const ProgressContainer = () => {
   return (
     <View style={styles.progressContainer}>
-      <Progress />
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => (
+          <Progress
+            title={item.title}
+          />
+        )}
+        keyExtractor={item => item.id}
+        numColumns='2'
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   progressContainer: {
-    flex: 1,
     backgroundColor: 'coral',
-  }
+    alignItems: 'center',
+  },
 });
 
 export default ProgressContainer;
